c4ded0eb8293c9e4d8046e68f91428156b0537a4
imufiid <imufiid98@gmail.com>
2020-11-30 12:25:11 +0700

    create helper response API

--- application/helpers/responses_helper.php
+++ application/helpers/responses_helper.php
@@ -1,6 +1,21 @@
 <?php
 
+use const chriskacerguis\RestServer\HTTP_UNAUTHORIZED;
 
-function responses() {
-   
+function successResponse($status = HTTP_UNAUTHORIZED, $message = "", $data = null) {
+   $ci =& get_instance();
+   if($data == null) {
+      $ci->response([
+         'status' => $status,
+         'message' => $message,
+      ], $status);
+      
+   } else {
+      $ci->response([
+         'status' => $status,
+         'message' => $message,
+         'data' => $data
+      ], $status);
+
+   }
 }
